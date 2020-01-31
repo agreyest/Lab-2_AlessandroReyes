1293056431c01c8e04075a8848762029c4cd31bb
agreyest <56709605+agreyest@users.noreply.github.com>
2020-01-31 14:04:52 -0600

    clase personaje
    agregado unos cambios y el to string

--- Lab#2_AlessandroReyes/src/lab/pkg2_alessandroreyes/Lab2_AlessandroReyes.java
+++ Lab#2_AlessandroReyes/src/lab/pkg2_alessandroreyes/Lab2_AlessandroReyes.java
@@ -22,26 +22,99 @@ public class Lab2_AlessandroReyes {
                     + "\n6) Salir");
             int op = leer.nextInt();
             switch(op){
-                case 1:
+                case 1://crear
+                    int tipo =0;
                     System.out.println("Que tipo desea jugar (presione un numero): "
                             + "\n1) Clerigo"
                             + "\n2) Barbaro"
                             + "\n3) Mago"
                             + "\n4) Picaro");
-                    break;
-                case 2:
+                    tipo = leer.nextInt();
+                    while (tipo < 1 || tipo > 4) {
+                        System.out.println("Lo ingresado esta malo, vuelva a intentar"
+                                + "\nQue tipo desea jugar (presione un numero): "
+                                + "\n1) Clerigo"
+                                + "\n2) Barbaro"
+                                + "\n3) Mago"
+                                + "\n4) Picaro");
+                        tipo = leer.nextInt();
+                    }
+                    System.out.println("Ingrese el nombre de su personaje: ");
+                    String nombre = leer.nextLine();
+                    boolean ver = false;
+                    for (int i = 0; i < personajes.size(); i++) {
+                        if(personajes.get(i).getNombre().equals(nombre)){
+                            ver = true;
+                        }
+                    }
+                    while (ver) {
+                        System.out.println("El nombre que ingreso ya existe"
+                                + "\nIngrese el nombre de su personaje: ");
+                        nombre = leer.nextLine();
+                        for (int i = 0; i < personajes.size(); i++) {
+                            if (personajes.get(i).getNombre().equals(nombre)) {
+                                ver = true;
+                                break;
+                            }else
+                                ver = false;
+                        }
+                    }
+                    System.out.println("Que raza es su jugador (presione un numero): "
+                            + "\n1) Mediano"
+                            + "\n2) Enano"
+                            + "\n3) Elfo"
+                            + "\n4) Humano");
+                    int raza = leer.nextInt();
+                    while (raza < 1 || raza > 4) {
+                        System.out.println("Lo ingresado esta malo, vuelva a intentar"
+                                + "\nQue raza es su jugador (presione un numero): "
+                                + "\n1) Mediano"
+                                + "\n2) Enano"
+                                + "\n3) Elfo"
+                                + "\n4) Humano");
+                        raza = leer.nextInt();
+                    }
+                    System.out.println("Ingrese la altura de su personaje: ");
+                    int altura = leer.nextInt();
+                    System.out.println("Ingrese el peso de su personaje: ");
+                    int peso = leer.nextInt();
+                    System.out.println("Ingrese la edad de su personaje: ");
+                    int edad = leer.nextInt();
+                    System.out.println("Ingrese una leve descripcion de su personaje ");
+                    String desc = leer.nextLine();
+                    
+                    System.out.println("Que nacionalidad es su jugador (presione un numero): "
+                            + "\n1) Norfair"
+                            + "\n2) Brinstar"
+                            + "\n3) Maridia"
+                            + "\n4) Zebes"
+                            + "\n5) Crateria");
+                    int nat = leer.nextInt();
+                    while (nat < 1 || nat > 5) {
+                        System.out.println("Lo ingresado esta malo, vuelva a intentar"
+                                + "\nQue nacionalidad es su jugador (presione un numero): "
+                                + "\n1) Norfair"
+                                + "\n2) Brinstar"
+                                + "\n3) Maridia"
+                                + "\n4) Zebes"
+                                + "\n5) Crateria");
+                        nat = leer.nextInt();
+                    }
                     
                     break;
-                case 3:
+                case 2://modificar
                     
                     break;
-                case 4:
+                case 3://vér
                     
                     break;
-                case 5:
+                case 4://elimnar
                     
                     break;
-                case 6:
+                case 5://combate
+                    
+                    break;
+                case 6://salir
                     resp = 'n';
                     break;
                 default:
--- Lab#2_AlessandroReyes/src/lab/pkg2_alessandroreyes/Personaje.java
+++ Lab#2_AlessandroReyes/src/lab/pkg2_alessandroreyes/Personaje.java
@@ -3,18 +3,22 @@ package lab.pkg2_alessandroreyes;
 public class Personaje {
     private int tipo;
     private String nombre;
-    private String raza;
+    private int raza;
     private String estatura;
     private int peso;
     private int edad;
     private String descripcion;
-    private String nacionalidad;
+    private int nacionalidad;
+    private int hp;
+    private int cs;
+    private int ac;
+    private int dg;
 
     public Personaje() {
     
     }
 
-    public Personaje(int tipo, String raza, String estatura, int peso, int edad, String descripcion, String nacionalidad) {
+    public Personaje(int tipo, int raza, String estatura, int peso, int edad, String descripcion, int nacionalidad) {
         if(tipo >0 && tipo < 5)
             this.tipo = tipo;
         this.raza = raza;
@@ -43,11 +47,11 @@ public class Personaje {
     }
 
     
-    public String getRaza() {
+    public int getRaza() {
         return raza;
     }
 
-    public void setRaza(String raza) {
+    public void setRaza(int raza) {
         this.raza = raza;
     }
 
@@ -83,13 +87,51 @@ public class Personaje {
         this.descripcion = descripcion;
     }
 
-    public String getNacionalidad() {
+    public int getNacionalidad() {
         return nacionalidad;
     }
 
-    public void setNacionalidad(String nacionalidad) {
+    public void setNacionalidad(int nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getCs() {
+        return cs;
+    }
+
+    public void setCs(int cs) {
+        this.cs = cs;
+    }
+
+    public int getAc() {
+        return ac;
+    }
+
+    public void setAc(int ac) {
+        this.ac = ac;
+    }
+
+    public int getDg() {
+        return dg;
+    }
+
+    public void setDg(int dg) {
+        this.dg = dg;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre= " + nombre + "\n hp= " + hp + "\n cs= " + cs + "\n ac= " + ac + "\n dg= " + dg;
+    }
+    
     
     
 }
